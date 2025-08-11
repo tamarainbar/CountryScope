6d2dd3ff6fa32409cc4a793a0a48f98e75876964
Tamara Inbar <tamarainbar@gmail.com>
2025-08-11 15:30:51 -0400

    added quiz and learn templetes

---
+++ CountryScope/Learn.swift
@@ -0,0 +1,18 @@
+//
+//  Learn.swift
+//  CountryScope
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+struct Learn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Learn()
+}
---
+++ CountryScope/Quiz.swift
@@ -0,0 +1,18 @@
+//
+//  Quiz.swift
+//  CountryScope
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+struct Quiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quiz()
+}
