21bffd50b80820b04b336cd8c59b3f0e33f8aaf9
Tamara Inbar <tamarainbar@gmail.com>
2025-08-11 15:16:12 -0400

    Initial Commit

---
+++ CountryScope/CountryScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryScopeApp.swift
+//  CountryScope
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
