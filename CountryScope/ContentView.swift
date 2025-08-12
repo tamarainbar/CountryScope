27a09aa7d793b86649f87ed09a207c447486f4d7
Lara <laragandre@gmail.com>
2025-08-12 11:23:17 -0400

    Added title and background colors(not finished though)

--- CountryScope/ContentView.swift
+++ CountryScope/ContentView.swift
@@ -9,27 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        NavigationStack {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Welcome!")
-                
-                NavigationLink(destination: Learn())
-                {
-                    Text("Learn")
-                }
-                
-                NavigationLink(destination: Quiz()) {
-                    Text("Quiz")
-                }
-                
-                
-            } // end of vstack
-            .padding()
-        } // end of nav stack
+            
+            NavigationStack {
+                ZStack{
+                    Color.mint
+                        .ignoresSafeArea(edges: .all)
+                    VStack {
+                        //  Text("hi")
+                        //  .multilineTextAlignment(.leading)
+                        Text("Country")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                        
+                        
+                        
+                        
+                        
+                        Text("Kaleidoscope")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.trailing)
+                        
+                        Spacer()
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundStyle(.tint)
+                        Text("Welcome!")
+                        
+                        NavigationLink(destination: Learn())
+                        {
+                            Text("Learn")
+                        }
+                        
+                        NavigationLink(destination: Quiz()) {
+                            Text("Quiz")
+                            
+                        }
+                        Spacer()
+                        
+                    } // end of vstack
+                    .padding()
+                }//end of ZStack
+            } // end of nav stack
     }
 }
 
