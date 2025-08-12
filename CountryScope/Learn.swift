69f4b9fca95f83d26098458c653683cb489a85b6
Sophie <thanjusophie@gmail.com>
2025-08-12 11:48:57 -0400

    Adding markers to map

--- CountryScope/Learn.swift
+++ CountryScope/Learn.swift
@@ -10,17 +10,33 @@ import MapKit
 
 struct Learn: View {
     var body: some View {
-        Map
-        {
+        Map {
+            // Example markers
+            Marker("United States", coordinate: CLLocationCoordinate2D(latitude: 37.0902, longitude: -95.7129))
+            
+            Marker("Canada", coordinate: CLLocationCoordinate2D(latitude: 56.1304, longitude: -106.3468))
+            
+            Marker("Mexico", coordinate: CLLocationCoordinate2D(latitude: 23.6345, longitude: -102.5528))
+            
+            Marker("Argentina", coordinate: CLLocationCoordinate2D(latitude: -38.4161, longitude: -63.616))
+            
+            Marker("Brazil", coordinate: CLLocationCoordinate2D(latitude: -14.2350, longitude: -51.9253))
+            
+            Marker("Peru", coordinate: CLLocationCoordinate2D(latitude: -9.1900, longitude: -75.0152))
+            
+            Marker("Bolivia", coordinate: CLLocationCoordinate2D(latitude: -16.2902, longitude: -63.5887))
+            
+            Marker("", coordinate: CLLocationCoordinate2D(latitude: 56.1304, longitude: -106.3468))
+        
+            Marker("", coordinate: CLLocationCoordinate2D(latitude: 56.1304, longitude: -106.3468))
+            
+            Marker("", coordinate: CLLocationCoordinate2D(latitude: 56.1304, longitude: -106.3468))
             
         }
-        //.mapStyle(.mutedStandard)
         .mapControlVisibility(.hidden)
         .mapStyle(.imagery(elevation: .realistic))
     }
 }
-
-
 
 #Preview {
     Learn()
