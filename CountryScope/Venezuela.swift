01183ece7e2dc1cd1a26f5c91027365be5782317
Sophie <thanjusophie@gmail.com>
2025-08-13 13:12:29 -0400

    Made new views for all the countries

---
+++ CountryScope/Venezuela.swift
@@ -0,0 +1,18 @@
+//
+//  Venezuela.swift
+//  CountryScope
+//
+//  Created by Scholar on 8/13/25.
+//
+
+import SwiftUI
+
+struct Venezuela: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Venezuela()
+}
