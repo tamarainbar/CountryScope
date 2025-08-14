2ad50b93064d043541cc71563e59b098f34092aa
Charlotte Morales <charlie09hm@gmail.com>
2025-08-14 14:44:54 -0400

    added guyana

--- CountryScope/Guyana.swift
+++ CountryScope/Guyana.swift
@@ -10,20 +10,86 @@ import SwiftUI
 struct Guyana: View {
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
+            VStack(alignment: .leading , spacing: -18.0) {
                 // Title
-                Text("                    Guyana")
-                    .font(.largeTitle)
-                    .foregroundColor(Color(hue: 0.304, saturation: 0.611, brightness: 0.592))
-                    .multilineTextAlignment(.center)
+                Text("        Guyana")
+                    .font(.custom(
+                        "American Typewriter",
+                        fixedSize:45))          .foregroundColor(Color("guyanaGreen"))
+                    .multilineTextAlignment(.leading)
                     .bold()
                 Image("GuyanaFlag")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                    .frame(width: 350, height: 310)
+                ZStack{
+                    RoundedRectangle(cornerRadius: 9)
+                        .fill(Color.guyanaGreen)
+                        .frame(width: 350, height: 180)
+                    RoundedRectangle(cornerRadius: 9)
+                        .fill(Color.white)
+                        .frame(width: 320, height: 150)
+                    VStack{//VStack 1
+                        HStack{//HStack 1
+                            Text("Capital:")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Georgetown")}
+                        HStack{
+                            Text("Currency:")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Guyanese Dollar")
+                        }
+                        HStack{
+                            Text("Language:")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("English")
+                        }
+                        HStack{
+                            Text("Popular Landmark:")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("1763 Monument")
+                        }
+                        HStack{
+                            Text("National Flower:")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Giant water lily")
+                        }
+                        HStack{
+                            Text("National Dish:")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Pepperpot")
+                        }
+                    }//end of VStack 1
+                    .font(.custom(
+                        "Times New Roman",
+                        fixedSize:20))
+                }//end of ZStack
+                .padding(.bottom)
             }
+            Text("National Animal")
+                .font(.custom(
+                    "Futura",
+                    fixedSize:25))
+            Text("Jaguar")
+            ZStack{//ZStack 1
+                RoundedRectangle(cornerRadius: 9)
+                    .fill(Color.guyanaGreen)
+                    .frame(width: 330, height: 230)
+                Image("guyanaJaguar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 290)
+                
+            }//end of scroll
         }
     }
-    }
+}
 #Preview {
     Guyana()
 }
