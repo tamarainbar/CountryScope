fad972473c0fb32e70253ecd6c5f76ca92b5a0c7
Lara <laragandre@gmail.com>
2025-08-14 09:51:24 -0400

    Completed Brazil UI template :D

--- CountryScope/Brazil.swift
+++ CountryScope/Brazil.swift
@@ -15,9 +15,8 @@ import SwiftUI
 struct Brazil: View {
   var body: some View {
     ScrollView {
-      VStack(alignment: .leading, spacing: 10.0) {
-        // Title
-        Text("         Brazil")
+      VStack(alignment: .leading, spacing: -25.0) {
+      Text("         Brazil")
           .font(.custom(
            "American Typewriter",
            fixedSize:45))          .foregroundColor(Color(hue: 0.358, saturation: 0.309, brightness: 0.524))
@@ -26,15 +25,15 @@ struct Brazil: View {
         Image("brazilFlag")
           .resizable()
           .aspectRatio(contentMode: .fit)
-          .padding(.bottom, 50)
+        //  .padding(.bottom, 50)
           .frame(width: 350, height: 350)
         ZStack{
           RoundedRectangle(cornerRadius: 9)
             .fill(Color.brazilgreen)
-            .frame(width: 350, height: 110)
+            .frame(width: 350, height: 150)
           RoundedRectangle(cornerRadius: 9)
             .fill(Color.white)
-            .frame(width: 320, height: 80)
+            .frame(width: 320, height: 120)
           VStack{//VStack 1
             HStack{//HStack 1
               Text("Capital:")
@@ -53,20 +52,40 @@ struct Brazil: View {
                 .fontWeight(.bold)
               Text("Portuguese")
             }
+              HStack{
+                Text("National Dish:")
+                  .font(.headline)
+                  .fontWeight(.bold)
+                Text("Feijoada")
+              }
+              HStack{
+                Text("National Flower:")
+                  .font(.headline)
+                  .fontWeight(.bold)
+                Text("Ipe-Amarelo")
+              }
           }//end of VStack 1
           .font(.custom(
            "Times New Roman",
            fixedSize:20))
         }//end of ZStack
         .padding(.bottom)
-        Text("Popular Landmarks")
-        ZStack{//ZStack 1
-         RoundedRectangle(cornerRadius: 9)
-            .fill(Color.brazilgreen)
-            .frame(width: 150, height: 150)
-        }//end of ZStack 1
+        }
       }
-    }
+      Text("Popular Landmarks")
+          .font(.custom(
+           "Futura",
+           fixedSize:25))
+      Text("Christ the Redeemer Statue")
+      ZStack{//ZStack 1
+       RoundedRectangle(cornerRadius: 9)
+          .fill(Color.brazilgreen)
+          .frame(width: 270, height: 190)
+          Image("brazilLandmark")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+          .frame(width: 290, height: 170)
+    }//end of ZStack 1
       }
     }
 #Preview {
